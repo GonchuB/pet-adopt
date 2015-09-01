67a1502b880e4efb62d7d62dd04da7229e5426ac
Joaquin Stankus <joaquin.stankus@fdvsolutions.com>
2015-09-01 20:04:33 -0300

    This is a combination of 4 commits. Initial progress in facebook login
    Changed to cleaner implementation
    
    Deleted unnecessary class
    
    Showing an activity communicated by an intent on successful fb login

---
+++ PetAdopt/app/src/main/java/com/fiuba/tdp/petadopt/AdoptionSearchActivity.java
@@ -0,0 +1,35 @@
+package com.fiuba.tdp.petadopt;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by joaquinstankus on 02/09/15.
+ */
+public class AdoptionSearchActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Get the message from the intent
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MyActivity.EXTRA_MESSAGE);
+
+        // Create the text view
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(message);
+
+        // Set the text view as the activity layout
+        setContentView(textView);
+    }
+
+}
