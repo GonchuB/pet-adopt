02f50f6c9db6a405e066339515517def4ee2ae0f
Tomás Boccardo <tomasboccardo@gmai.com>
2015-09-27 17:48:09 -0300

    Advance search view

--- app/src/main/java/com/fiuba/tdp/petadopt/fragments/search/AdvancedSearchFragment.java
+++ app/src/main/java/com/fiuba/tdp/petadopt/fragments/search/AdvancedSearchFragment.java
@@ -2,25 +2,73 @@ package com.fiuba.tdp.petadopt.fragments.search;
 
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.fiuba.tdp.petadopt.R;
+import com.fiuba.tdp.petadopt.fragments.addPet.map.ChooseLocationMapFragment;
+import com.fiuba.tdp.petadopt.fragments.addPet.map.LocationChosenDelegate;
+import com.fiuba.tdp.petadopt.model.Pet;
+import com.google.android.gms.maps.model.LatLng;
 
 public class AdvancedSearchFragment extends Fragment {
-    public AdvancedSearchFragment(){}
+    private Pet pet;
+    private View rootView;
+    public AdvancedSearchFragment(){
+        pet = new Pet();
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        View rootView = inflater.inflate(R.layout.fragment_advanced_search, container, false);
+        rootView = inflater.inflate(R.layout.fragment_advanced_search, container, false);
         setHasOptionsMenu(true);
 
+        populateSpinner(rootView, R.id.pet_type, R.array.pet_type_array);
+        populateSpinner(rootView, R.id.pet_gender, R.array.pet_gender_array);
+        populateSpinner(rootView, R.id.pet_main_color, R.array.pet_color_array);
+        populateSpinner(rootView, R.id.pet_second_color, R.array.pet_color_array);
+        TextView locationView = (TextView) rootView.findViewById(R.id.chosen_location);
+        final Button button = (Button) rootView.findViewById(R.id.choose_location);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ChooseLocationMapFragment mapFragment = new ChooseLocationMapFragment();
+                mapFragment.setLocationChosenDelegate(new LocationChosenDelegate() {
+                    @Override
+                    public void locationWasChosen(LatLng location, String address) {
+                        pet.setLocation(location);
+                        TextView locationView = (TextView) rootView.findViewById(R.id.chosen_location);
+                        locationView.setText(address);
+                    }
+                });
+                FragmentTransaction ft = getFragmentManager().beginTransaction();
+                ft.add(R.id.content_frame, mapFragment, "Choose location");
+                ft.addToBackStack(null);
+                ft.commit();
+            }
+        });
+
         return rootView;
+    }
+
+    private void populateSpinner(View rootView, int viewId, int arrayId) {
+        Spinner spinner = (Spinner) rootView.findViewById(viewId);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(),
+                arrayId, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
     }
 
     @Override
