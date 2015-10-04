7388374ec8cb36d26f5d8da1e49152aef298b440
Tomás Boccardo <tomasboccardo@gmai.com>
2015-10-04 17:56:56 -0300

    Some progress on Add pet screen

--- app/src/main/java/com/fiuba/tdp/petadopt/fragments/addPet/AddPetFragment.java
+++ app/src/main/java/com/fiuba/tdp/petadopt/fragments/addPet/AddPetFragment.java
@@ -25,7 +25,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
-import android.widget.EditText;
+import com.rey.material.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
