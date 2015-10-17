286315ddc3104712796e358ebde767af49689fe2
Tomas Boccardo <tomasboccardo@gmail.com>
2015-10-17 13:40:35 -0300

    Little progress

--- app/src/main/java/com/fiuba/tdp/petadopt/fragments/MyPetsFragment.java
+++ app/src/main/java/com/fiuba/tdp/petadopt/fragments/MyPetsFragment.java
@@ -8,11 +8,22 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import com.fiuba.tdp.petadopt.R;
 
 public class MyPetsFragment extends ResultFragment {
 
     public MyPetsFragment(){}
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        final View rootView = inflater.inflate(R.layout.fragment_mypets, container, false);
+        lv = (ListView) rootView.findViewById(R.id.pet_list);
+
+        return rootView;
+    }
 
 }
