0c3e4c2875c4aa5d63619e7c0996e4295605a504
lucasSimonelli <lucasp.simonelli@gmail.com>
2015-10-31 17:04:03 -0300

    Swapped Requested & My pets filling methods

--- app/src/main/java/com/fiuba/tdp/petadopt/fragments/MyPetsFragment.java
+++ app/src/main/java/com/fiuba/tdp/petadopt/fragments/MyPetsFragment.java
@@ -1,9 +1,37 @@
 package com.fiuba.tdp.petadopt.fragments;
 
-/**
- * Created by joaquinstankus on 07/09/15.
- */
+import android.support.v4.app.FragmentTransaction;
+import android.view.View;
+import android.widget.AdapterView;
+
+import com.fiuba.tdp.petadopt.R;
+import com.fiuba.tdp.petadopt.service.PetsClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.apache.http.Header;
+import org.json.JSONArray;
 
 public class MyPetsFragment extends PetResultFragment {
-    public MyPetsFragment(){}
+    public MyPetsFragment(){ onItemClickHandler = new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            String selectedId = pets.get(position).getId();
+            final AdopterResultFragment adopterResultFragment = new AdopterResultFragment();
+            adopterResultFragment.setPet(pets.get(position));
+            PetsClient client = PetsClient.instance();
+            client.getAdoptersForPet(selectedId, new JsonHttpResponseHandler() {
+                @Override
+                public void onSuccess(int code, Header[] headers, JSONArray body) {
+                    adopterResultFragment.setResults(body);
+                    adopterResultFragment.onStart();
+                }
+            });
+            getActivity().setTitle(pets.get(position).getName() + " - " + getActivity().getString(R.string.requesters_title));
+            FragmentTransaction ft = getFragmentManager().beginTransaction();
+            ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+            ft.add(R.id.content_frame, adopterResultFragment, "Adopter Result Fragment");
+            ft.addToBackStack(null);
+            ft.commit();
+        }
+    };}
 }
